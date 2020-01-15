1f0bb72f48be02eb3f9b92ad37c35cf8246d4b3d
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-01-14 16:30:03 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,27 @@
 
-let cards = ["queen", "king", "queen","king"];
+let cards = ["queen", "queen", "king","king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen ");
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-console.log('User flipped king');
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[2]) {
-    alert("You found a match!")
-  } else if (cardsInPlay[0] !== cardsInPlay[4]) {
-    alert("Sorry Try Again!")
-  } else {
-    console.log("nothing");
-  }
+function checkForMatch() {
+   if (cardsInPlay.length === 2) {
+     if (cardsInPlay[0] === cardsInPlay[1]) {
+       alert("You found a match!");
+     } else if (cardsInPlay[0] !== cardsInPlay[4]) {
+       alert("Sorry Try Again!");
+     } else {
+       console.log("nothing");
+     }
+   }
 }
+
+function flipCard(cardId) {
+
+  checkForMatch();
+ 
+  console.log("User flipped " + cards[cardId]);
+
+  cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
 
